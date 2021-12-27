4070ec81f4ef7baf1b51993e7c3f3f2c9288f5c5
Nabila <nsalshabila15@gmail.com>
2021-12-27 22:07:47 +0700

    tambah nabila

--- main.cpp
+++ main.cpp
@@ -29,3 +29,38 @@ GLfloat qPos[]={0,0,0,0.1};
 GLfloat sc[8]={0.295 , 0.40,0.50, 0.60,0.80,1.0,1.05,1.13};
 double ang=2*PI/300;
 double angular=2*PI/50;
+
+void initLighting()
+{
+    //glMaterialfv(GL_FRONT,GL_AMBIENT,yellow);
+    //glMaterialfv(GL_FRONT,GL_SPECULAR,yellow);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT7);
+
+    glLightfv(GL_LIGHT7,GL_AMBIENT,qAmb);
+    glLightfv(GL_LIGHT7,GL_DIFFUSE,qDif);
+    glLightfv(GL_LIGHT7,GL_SPECULAR,qSpec);
+    //glMaterialfv(GL_FRONT,GL_DIFFUSE,yellow);
+}
+void myinit()
+{
+   glClearColor(0.0,0.0,0.0,0.0); //backgroundcolor is green
+   //gluOrtho2D(0,699,0,699);
+   glPointSize(1.0);
+   glLineWidth(2.0);
+
+}
+
+void background()
+{
+    glBegin(GL_QUADS);
+      glColor3f(0.0,0.00,0.00);
+      glVertex3f(-01.00,01.00,1);
+      glColor3f(.20,0.0,0.70);
+      glVertex3f(01.00,1.00,1);
+      glColor3f(0,0.0,0.0);
+      glVertex3f(1.00,-1.00,1);
+      glColor3f(.70,.10,.20);
+      glVertex3f(-1.00,-1.00,1);
+    glEnd();
+}
